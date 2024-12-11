b5dd48baaddd4718030b7441cbdf5e989599186d
SymphonyZhang <95200261@qq.com>
2024-12-11 18:14:07 +0800

    完成了播放功能

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # MediaExoplayerSample
 Media3 ExoPlayer Sample
 
-一个基于 Media3 ExoPlayer 的视频播放器的示例
+一个基于 Media3 ExoPlayer 的视频播放器的简单示例
 AndroidStudio-Ladybug
 Media3 ExoPlayer 当前版本为 1.5.0
+media.exolist.json:视频播放地址及其他信息
--- app/src/main/java/com/symphony/mediaexoplayersample/MainActivity.kt
+++ app/src/main/java/com/symphony/mediaexoplayersample/MainActivity.kt
@@ -5,16 +5,50 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import com.symphony.mediaexoplayersample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        // 实例化player
+        val player = ExoPlayer.Builder(this).build()
+        // 将player添加到playerView中
+        binding.playerView.player = player
+        // 视频播放地址
+        val videoUrl = "https://storage.googleapis.com/wvmedia/clear/h264/tears/tears.mpd"
+        //设置mediaItem给player
+        val mediaItem = MediaItem.fromUri(videoUrl)
+        player.setMediaItem(mediaItem)
+        //player准备好
+        player.prepare()
     }
+
+    override fun onStart() {
+        super.onStart()
+        binding.playerView.onResume()
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        binding.playerView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.playerView.player?.release()
+    }
+
 }
